3377f1e19aa7fb25a29a0353d2e0eeb5f1581dd8
nikagelo <nikagelo@gmail.com>
2023-02-15 18:00:32 +0400

    copied from past repository because of  bugs. from test1 to test11

---
+++ src/main/java/DataObject/MainPageData.java
@@ -0,0 +1,8 @@
+package DataObject;
+
+public interface MainPageData {
+//    რა უნდა ჩაიწეროს ცარიელ ველში
+    String searchItemData = "კალამი ბურთულიანი",
+    SearchTestData = "satesto";
+    Integer num = 3;
+}
---
+++ src/main/java/PageObject/MainPage.java
@@ -0,0 +1,31 @@
+package PageObject;
+
+
+import com.codeborne.selenide.SelenideElement;
+
+import static DataObject.MainPageData.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class MainPage {
+    public SelenideElement searchBtn = $(byClassName("fa-search")),
+    searchInput = $(byName("search")),
+    searchResult = $(byText(searchItemData)),
+    nothingFound = $(byText("ვერაფერი მოიძებნა")),
+    addToCart = $(byClassName("add_cart"), 0),
+    addedItemTxt = $(byText("პროდუქტი წარმატებით დაემატა კალათაში")),
+    goToCart  = $(byClassName("junior-span")),
+    itemTitle = $(byClassName("item_title")),
+    itemColor = $(byClassName("choose_color")),
+    itemID = $(byClassName("item_id")),
+    itemNum = $(byName("quantity")),
+    itemPrice = $(byXpath("/html/body/div[5]/div/div[3]/div[6]/div[2]/span[1]")),
+    itemFullPrice = $(byClassName("fullPriceFor")),
+    plusBtn = $(byClassName("fa-plus")),
+    minusBtn = $(byClassName("fa-minus"));
+
+
+
+
+
+}
